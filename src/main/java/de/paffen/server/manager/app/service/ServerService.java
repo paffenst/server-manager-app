d4b5abf635df5334f70bd9a585c829db53aebca4
paffenst <paffen.st@gmail.com>
2023-09-04 17:08:43 +0200

    mapping HTTP Response

--- src/main/java/de/paffen/server/manager/app/service/ServerService.java
+++ src/main/java/de/paffen/server/manager/app/service/ServerService.java
@@ -2,11 +2,12 @@ package de.paffen.server.manager.app.service;
 
 import de.paffen.server.manager.app.model.Server;
 
+import java.io.IOException;
 import java.util.Collection;
 
 public interface ServerService {
     Server create(Server server);
-    Server ping(String ipAddress);
+    Server ping(String ipAddress) throws IOException;
     Collection<Server> list(int limit);
     Server get(Long id);
     Server update(Server server);
--- src/main/java/de/paffen/server/manager/app/service/implementation/ServerServiceImpl.java
+++ src/main/java/de/paffen/server/manager/app/service/implementation/ServerServiceImpl.java
@@ -1,14 +1,22 @@
 package de.paffen.server.manager.app.service.implementation;
 
+import de.paffen.server.manager.app.enumeration.Status;
 import de.paffen.server.manager.app.model.Server;
 import de.paffen.server.manager.app.repo.ServerRepo;
 import de.paffen.server.manager.app.service.ServerService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
+
+import static de.paffen.server.manager.app.enumeration.Status.SERVER_DOWN;
+import static de.paffen.server.manager.app.enumeration.Status.SERVER_UP;
+import static java.lang.Boolean.TRUE;
 
 @RequiredArgsConstructor
 @Service
@@ -25,28 +33,39 @@ public class ServerServiceImpl implements ServerService {
     }
 
     @Override
-    public Server ping(String ipAddress) {
-        return null;
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}", ipAddress);
+        Server server = serverRepo.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
+        serverRepo.save(server);
+        return server;
     }
 
     @Override
     public Collection<Server> list(int limit) {
-        return null;
+        log.info("Fetching all servers");
+        return serverRepo.findAll(PageRequest.of(0,limit)).toList();
+
     }
 
     @Override
     public Server get(Long id) {
-        return null;
+        log.info("Fetching server by id:{}",id);
+        return serverRepo.findById(id).get();
     }
 
     @Override
     public Server update(Server server) {
-        return null;
+        log.info("Updating server: {}",server.getName());
+        return serverRepo.save(server);
     }
 
     @Override
     public Boolean delete(Long id) {
-        return null;
+        log.info("Deleting server by ID: {}",id );
+        serverRepo.deleteById(id);
+        return TRUE;
     }
 
     private String setServerImageUrl() {
